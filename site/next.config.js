54f0232e301960d942e453a80951fb9203ce3219
Harun Kilic <harunkilic@live.dk>
2025-04-02 12:40:05 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Novuslab',
+    description: 'Custom workspaces for development.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/HarunKilic/kasm-registry/',
+    contactUrl: 'https://github.com/HarunKilic/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
